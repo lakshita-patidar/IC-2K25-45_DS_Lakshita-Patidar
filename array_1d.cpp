93a58dc102db23663672abb9433bfe520534bb58
lakshita-patidar <lakshitapatidar8@gmail.com>
2026-08-12 11:55:06 +0530

    Add files via upload

---
+++ array_1d.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter number of elements in array: ";
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cout<<i<<" ";
+    }
+}
